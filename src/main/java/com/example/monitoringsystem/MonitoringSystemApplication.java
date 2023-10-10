a574dcc4ac63387d888d65360caf79163522ebc2
Sirojiddin <siroj.abdumavlonov369@gmail.com>
2023-10-10 17:18:30 +0500

    nachalo

---
+++ src/main/java/com/example/monitoringsystem/MonitoringSystemApplication.java
@@ -0,0 +1,14 @@
+package com.example.monitoringsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoringSystemApplication.class, args);
+        //hello
+    }
+
+}
---
+++ src/test/java/com/example/monitoringsystem/MonitoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monitoringsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
