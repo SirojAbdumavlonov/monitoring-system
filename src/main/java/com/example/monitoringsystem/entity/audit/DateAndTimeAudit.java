d31759ac0629099c25ad4895a4d49f73091e906c
Sirojiddin <siroj.abdumavlonov369@gmail.com>
2023-11-06 00:47:55 +0500

    report continued!

---
+++ src/main/java/com/example/monitoringsystem/entity/audit/DateAndTimeAudit.java
@@ -0,0 +1,33 @@
+package com.example.monitoringsystem.entity.audit;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = {"createdDate","createdTime"},
+        allowGetters = true
+)
+public abstract class DateAndTimeAudit implements Serializable {
+
+    @CreatedDate
+    @JsonIgnore
+    private LocalDate createdDate;
+
+    @CreationTimestamp
+    @JsonIgnore
+    private LocalTime createdTime;
+}
